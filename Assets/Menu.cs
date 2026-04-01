6f81026d49a46edd9dacb393f059fd291c510001
plsimpson <plsimpson05@gmail.com>
2026-04-01 12:16:11 -0400

    Revert "Merge branch 'main' into Phill"
    This reverts commit 17787ae4bf7edfb1a2eb58219c9147fc92ab1669, reversing
    changes made to 4a2c732f2cdccf3d53bca98ef86a8e72bd8e5650.

--- Assets/Menu.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using UnityEngine.SceneManagement;  
-
-public class Menu: MonoBehaviour
-{
-    public void OnPlayButton ()
-    {         SceneManager.LoadScene("Tutorial");
-    }
-}
