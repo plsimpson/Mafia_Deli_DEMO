bf8ba8bbc98020bfdd529061987566c229a2b0cc
llettie67 <Lucas.J.Lettie@gmail.com>
2026-04-13 13:03:26 -0400

    character controller v4

---
+++ Assets/CharContrTest/PlayerScript.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerScript : MonoBehaviour
+{
+    [SerializeField] private PlayerInput playerInput;
+    [SerializeField] private CharacterController characterController;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 moveInput = playerInput.actions["Move"].ReadValue<Vector2>();
+        Debug.Log("Move input: " + moveInput);
+
+        characterController.Move(new Vector3(moveInput.x, 0, moveInput.y) * Time.deltaTime * 5f);
+        if (playerInput.actions["Jump"].triggered)
+        {
+           // EnterStation();
+        }
+    }
+
+}
