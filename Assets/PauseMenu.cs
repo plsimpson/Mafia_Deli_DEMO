32201824ec3f7e27c2f5e763847a1bcee31dc91d
llettie67 <Lucas.J.Lettie@gmail.com>
2026-04-20 16:58:45 -0400

    have to restart the pause menu
    pause menu being annoying going to ask bBlake

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        PauseMenu.setActive(false);
+    }
+
+    private static void setActive(bool v)
+    {
+        throw new NotImplementedException();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void PauseGame()
+    { }
+    PauseMenu.setActive(true);true);
+    }
+
