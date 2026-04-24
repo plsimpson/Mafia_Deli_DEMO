4c4b75a91f5fabdbf19be8c2ca61ed8b3eba9b4a
llettie67 <Lucas.J.Lettie@gmail.com>
2026-04-24 11:53:23 -0400

    Update EndTut.cs

--- Assets/Scripts/EndTut.cs
+++ Assets/Scripts/EndTut.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class EndTut : MonoBehaviour
+public class EndTut : DemoSceneLoader
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
