dcd8ecf14eb34b74880a2d0debc56d89255bbeae
hjplourde <haydenjplourde@gmail.com>
2026-04-28 11:46:14 -0400

    Better transition between scenes

--- Assets/HaydenScripts/PrototypeScripts/LoadSceneOnTriggerAndEnemyCount.cs
+++ Assets/HaydenScripts/PrototypeScripts/LoadSceneOnTriggerAndEnemyCount.cs
@@ -4,12 +4,10 @@ using UnityEngine.SceneManagement;
 public class LoadSceneOnTriggerAndEnemyCount : DemoSceneLoader
 {
     [SerializeField] private BoxCollider triggerCollider;
-    [SerializeField] private EnemyCount enemyCount;
-    //This will be a script soon
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player") && enemyCount.CurrentEnemyCount <= 0)
+        if (other.CompareTag("Player"))
         {
             SceneLoader();
         }
